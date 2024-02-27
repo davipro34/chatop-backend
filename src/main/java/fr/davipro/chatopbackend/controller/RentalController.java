cb8ed05d3fb4c111d227b8fee4183e11cb3066b8
David Atlan <david@davipro.fr>
2024-02-27 14:12:09 +0100

    Add Swagger annotations to RentalController.java to generate documentation

--- src/main/java/fr/davipro/chatopbackend/controller/RentalController.java
+++ src/main/java/fr/davipro/chatopbackend/controller/RentalController.java
@@ -9,13 +9,28 @@ import org.springframework.web.bind.annotation.RestController;
 import fr.davipro.chatopbackend.dto.RentalDTO;
 import fr.davipro.chatopbackend.dto.RentalsDTO;
 import fr.davipro.chatopbackend.service.RentalService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "Rental", description = "Rental management APIs")
 @RestController
 public class RentalController {
     
     @Autowired
     private RentalService rentalService;
 
+    @Operation(
+      summary = "Retrieve a list of all rentals",
+      description = "Get a list of rentals. The response is a list of Rental objects with an ID, name, surface, price, picture, description, ID owner, created date and updated date.",
+      tags = { "rentals", "get" })
+    @ApiResponses({
+      @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = RentalDTO.class), mediaType = "application/json") }),
+      @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+      @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @GetMapping("/api/rentals")
     public RentalsDTO getRentals() {
         RentalsDTO rentals = new RentalsDTO();
